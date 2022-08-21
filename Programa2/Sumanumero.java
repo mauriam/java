a242c522838a6093f7c288b0911d896d4a41f659
mauriam <106772464+mauriam@users.noreply.github.com>
2022-08-20 20:24:05 -0400

    Programa numero 2

---
+++ Programa2/Sumanumero.java
@@ -0,0 +1,11 @@
+package ParteI.Programa2;
+
+public class Sumanumero {
+    public static void main (String []Args){
+        int x=5;
+        int y=6;
+        System.out.println("la suma de "+x+"+"+y+" es "+(x+y));
+    }
+ 
+}
+  
